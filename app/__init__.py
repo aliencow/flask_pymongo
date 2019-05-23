e45ea4788f1fba4b64e99ddc7b0fb03f96aad953
Juan R Nouche <juan.nouche@gmail.com>
2019-05-23 11:54:11 +0000

    reestructuración jinja templates

---
+++ app/__init__.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+# para codificar en utf-8
+
+# basado en videotutorial en https://www.youtube.com/watch?v=rGER0KDdJqI
+# combinado con este otro https://www.youtube.com/watch?v=GjJbaolBPAY
+
+from flask import Flask
+# importamos Flask
+from flask_pymongo import PyMongo
+# importamos pymongo
+from flask_cors import CORS
+# para ejecutar codigo seguro
+from flask_bcrypt import Bcrypt
+# para encpriptar contraseñas etc
+import urllib
+# para obviar el error de la @ en el nombre de ususario
+
+app = Flask(__name__)
+# así se define la entrada de la aplicacion de Flask
+CORS(app)
+# para evitar intrusos cuestion de  (desactivado por el momento)
+bcrypt = Bcrypt(app)
+# activamos la variable de encriptación por lo que pueda pasar
+
+app.config['MONGO_DBNAME'] = 'connect_to_mongo'
+# configuramos el nombre de la base de datos se accede con la variable mongo.db
+
+app.config['MONGO_URI'] = 'mongodb://'+ urllib.quote("juan.nouche@gmail.com") +':cuatro44@ds143293.mlab.com:43293/connect_to_mongo'
+# configuramos la ubicacion de la base en mlab (aparece en pantalla)
+# aquí se usa el urllib para el tema de arroba en el nombre de usuario
+
+mongo = PyMongo(app)
+# conectamos con la base de datos nuestra app
+
+from app import routes
---
+++ app/routes.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+# para codificar en utf-8
+from flask import render_template
+from app import app, mongo
+
+
+@app.route('/')
+@app.route('/index')
+def index():
+    user = {'username': 'Miguel'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
+
+@app.route('/wibble')
+def wibble():
+    return 'This is my pointless new page'
+
+@app.route("/add")
+# ruta en la web desde el directorio base
+def add(): # definición de la función que se va a ejecutar desde esa ruta (no tiene porque coincidir el nombre)
+    user = mongo.db.users #crea una collection (tabla) users dentro de nuestra base de datos (si no existe ya)
+    user.insert({'name' : 'Manolete', 'Apellido':'AnyWhere'}) # insertamos un document (registro)
+    return 'Se ha añadido a Manolete'
